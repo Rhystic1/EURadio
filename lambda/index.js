a5d072342f00e5ba1b705b37997f707ba0a64fe1
Rhystic1 <Rhystic1@users.noreply.github.com>
2023-03-19 01:17:57 +0000

    Import from Dev Console
    First release

---
+++ lambda/index.js
@@ -0,0 +1,79 @@
+const Alexa = require('ask-sdk-core');
+
+const PlayRadioIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'PlayRadioIntent';
+  },
+  handle(handlerInput) {
+    const stationSlot = handlerInput.requestEnvelope.request.intent.slots.station;
+    let speechText;
+    let streamUrl;
+    let stationName; // declare stationName here
+    if (stationSlot && stationSlot.value) {
+      stationName = stationSlot.value.toLowerCase(); // assign value to stationName here
+      switch (stationName) {
+        case 'rai radio 1':
+          speechText = 'Playing RAI Radio 1';
+          streamUrl = 'https://icestreaming.rai.it/1.mp3';
+          break;
+        case 'rai radio 3':
+          speechText = 'Playing RAI Radio 3';
+          streamUrl = 'https://icestreaming.rai.it/3.mp3';
+          break;
+        case 'radiofreccia':
+          speechText = 'Playing Radiofreccia';
+          streamUrl = 'https://streamcdnb7-dd782ed59e2a4e86aabf6fc508674b59.msvdn.net/live/S3160845/D6MENOraq6Qy/chunklist_b128000.m3u8';
+          break;
+        default:
+          speechText = `Sorry, I don't know the station ${stationName}`;
+      }
+    } else {
+      speechText = `Sorry, I didn't catch which station you want to play`;
+    }
+
+    if (streamUrl) {
+      return handlerInput.responseBuilder
+        .speak(speechText)
+        .addAudioPlayerPlayDirective('REPLACE_ALL', streamUrl, stationName, 0)
+        .getResponse();
+    } else {
+      return handlerInput.responseBuilder
+        .speak(speechText)
+        .reprompt(speechText)
+        .getResponse();
+    }
+  }
+};
+
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+  },
+  handle(handlerInput) {
+    console.log('Inside LaunchRequestHandler');
+    return handlerInput.responseBuilder
+      .speak('Welcome to Rhystic Radio!')
+      .reprompt('Welcome to Rhystic Radio!')
+      .getResponse();
+  },
+};
+
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Goodbye!';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
+
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(CancelAndStopIntentHandler, LaunchRequestHandler, PlayRadioIntentHandler)
+    .lambda();
